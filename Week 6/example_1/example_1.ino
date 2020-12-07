d08ea8265f471268d0639a86848d994f172fb6f9
fakhrezany <73817885+fakhrezany@users.noreply.github.com>
2020-12-07 11:52:32 +0800

    Create example_1.ino

---
+++ Week 6/example_1/example_1.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  // put your setup code here, to run once:
+    unsigned char* ddrd   = (unsigned char*) 0x2A;
+    unsigned char* ocr0b  = (unsigned char*) 0x48;
+    unsigned char* tccr0a = (unsigned char*) 0x44;
+    unsigned char* tccr0b = (unsigned char*) 0x45;
+
+      *ddrd = 1 << 5;
+      *ocr0b = 127;
+      *tccr0a = 0b00100011;
+      *tccr0b = 0b00000011;
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
